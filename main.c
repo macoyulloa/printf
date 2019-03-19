82a15e58fe2ed7c583c0bb1511c461bc60cf25fd
Rodrigo Cruz <rodrigocruzayala@gmail.com>
2019-03-19 17:41:28 +0000

    Adding features point 1

--- main.c
+++ main.c
@@ -9,23 +9,47 @@
  */
 int main(void)
 {
-	char i = 'g';
+	int len, len2, n = 321, m = 245;
+	unsigned int ui;
+	void *addr;
+	char i = 'g', h = ' ';
 	char j[] = "cadena de caracteres";
-	char h = ' ';
-	int n = 321, m = 245;
 
+	len = _printf("Let's try to printf a simple sentence.\n");
+	len2 = printf("Let's try to printf a simple sentence.\n");
+	ui = (unsigned int)INT_MAX + 1024;
+	addr = (void *)0x7ffe637541f0;
 	_printf("Let's try to printf a simple sentence.\n");
 	_printf("%c\n", i);
- 	_printf("%s\n", j);
+	_printf("%s\n", j);
 	_printf("hola Male, %c, funcionó\n", i);
 	_printf("hola mi nombre, %s, gracias\n", j);
 	_printf("hola mi nombre, %c, gracias\n", h);
-	_printf("hola mi, gracias\n", h);
-	_printf("Hola maria%\n");
-	/**printf("Hola maria%\n");**/
 	_printf("hola mi, %d , gracias\n", n);
-	_printf("hola mi, %i , gracias\n", m);
 	_printf("%d , gracias\n", m);
 	_printf("%i\n", n);
+	_printf("Length:[%d, %i]\n", len, len);
+	printf("Length:[%d, %i]\n", len2, len2);
+	_printf("Negative:[%d]\n", -762534);
+	printf("Negative:[%d]\n", -762534);
+	_printf("Unsigned:[%u]\n", ui);
+	printf("Unsigned:[%u]\n", ui);
+	_printf("Unsigned octal:[%o]\n", ui);
+	printf("Unsigned octal:[%o]\n", ui);
+	_printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
+	printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
+	_printf("Character:[%c]\n", 'H');
+	printf("Character:[%c]\n", 'H');
+	_printf("String:[%s]\n", "I am a string !");
+	printf("String:[%s]\n", "I am a string !");
+	_printf("Address:[%p]\n", addr);
+	printf("Address:[%p]\n", addr);
+	len = _printf("Percent:[%%]\n");
+	len2 = printf("Percent:[%%]\n");
+	_printf("Len:[%d]\n", len);
+	printf("Len:[%d]\n", len2);
+	_printf("Unknown:[%r]\n");
+	printf("Unknown:[%r]\n");
+
 	return (0);
 }
