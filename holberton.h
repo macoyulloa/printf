62dd97411db7829e4b99feb615d435fdb618757f
Maria Alejandra Coy Ulloa <malejacoy@hotmail.com>
2019-03-20 03:31:41 +0000

    code reestructuration point 0

--- holberton.h
+++ holberton.h
@@ -1,9 +1,4 @@
 #ifndef Holberton
 #define Holberton
-int _putchar(char c);
-char *convert(unsigned int num, int base);
-int _count(char *s);
-char *_copy(char *src);
-void imp(char *s);
-int _printf(char *format, ...);
+int _printf(const char *format, ...);
 #endif
